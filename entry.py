66c2a00acdd2a4c3cdbe1accad60645ccf0ee355
kkeelltt <chapelh6@gmail.com>
2016-01-27 01:12:26 +0900

    half complete

--- entry.py
+++ entry.py
@@ -6,28 +6,29 @@ from bottle import route, run, template, request, app
 from beaker.middleware import SessionMiddleware
 from socket import gethostname, gethostbyname
 from datetime import datetime, timezone
+import pytz
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formatdate
 
 
-ENCODING = "ISO-2022-JP"
+ENCODING = "utf-8"
 FROM_ADDR = "kelt@club.kyutech.ac.jp"
+SMTP_SRV = "mail.club.kyutech.ac.jp"
 
-data = {}
 
 def create_msg(from_addr, to_addr, charset, subject, body):
-    msg = MIMEText(body.encode(charset), 'plain', charset)
+    msg = MIMEText(body, 'plain', charset)
     msg['Subject'] = Header(subject,charset)
     msg['From'] = from_addr
     msg['To'] = to_addr
     msg['Date'] = formatdate(localtime=True)
     return msg
 
-def send_msg(from_addr, to_addr, msg):
-    smtp = smtplib.SMTP()
-    smtp.connect()
+def send_msg(from_addr, to_addr, msg, host, port):
+    smtp = smtplib.SMTP(host, port)
+    smtp.ehlo()
     smtp.sendmail(from_addr, [to_addr], msg.as_string())
     smtp.close()
 
@@ -40,34 +41,33 @@ session_opts = {
 }
 
 
-@route('/')
+@route('/title')
 def title():
     return template('title')
 
 
 @route('/show', method='POST')
 def show():
-    for key, value in request.forms.allitems():
-        data[key] = value
-
     session = request.environ.get('beaker.session')
-    session['name_last'] = data['name_last'].encode('utf-8')
-    session['name_first'] = data['name_first'].encode('utf-8')
-    session['kana_last'] = data['kana_last'].encode('utf-8')
-    session['kana_first'] = data['kana_first'].encode('utf-8')
-    session['student_id'] = data['student_id']
-    session['isc_account'] = data['isc_account']
-    session['club_account'] = data['club_account']
+    #session['password'] = request.forms.password
+    session['name_last'] = request.forms.name_last
+    session['name_first'] = request.forms.name_first
+    session['kana_last'] = request.forms.kana_last
+    session['kana_first'] = request.forms.kana_first
+    session['student_id'] = request.forms.student_id
+    session['isc_account'] = request.forms.isc_account
+    session['club_account'] = request.forms.club_account
+    time = datetime.now(pytz.timezone('Asia/Tokyo'))
 
     return template('show',
-        name_last=data['name_last'],
-        name_first=data['name_first'],
-        kana_last=data['kana_last'],
-        kana_first=data['kana_first'],
-        student_id=data['student_id'],
-        isc_account=data['isc_account'],
-        club_account=data['club_account'],
-        time=datetime.now(timezone.utc).strftime('%Y/%m/%d %H:%M:%S %z'),
+        name_last=session['name_last'],
+        name_first=session['name_first'],
+        kana_last=session['kana_last'],
+        kana_first=session['kana_first'],
+        student_id=session['student_id'],
+        isc_account=session['isc_account'],
+        club_account=session['club_account'],
+        time=time.strftime('%Y-%m-%d %H:%M:%S %z'),
         hostname=gethostname(),
         remote_addr=request.remote_addr)
 
@@ -85,15 +85,14 @@ def mail():
     for_user += "以下のとおりアカウント申請を承りました。\n"
     for_user += "\n"
     for_user += "=================================================================\n"
-    for_user += "　氏名　　　　　 : " + data['name_last'] + data['name_first']
-    for_user += "(" + data['kana_last'] + data['kana_first'] + ")\n"
-    for_user += "　学生番号　　　 : " + data['student_id'] + "\n"
-    for_user += "　メールアドレス : " + data['isc_account'] + "@mail.kyutech.jp\n"
+    for_user += "　氏名　　　　　 : " + session['name_last'] + " " + session['name_first'] + " (" + session['kana_last'] + " " + session['kana_first'] + ")\n"
+    for_user += "　学生番号　　　 : " + session['student_id'] + "\n"
+    for_user += "　メールアドレス : " + session['isc_account'] + "@mail.kyutech.jp\n"
     for_user += "　-------------------------------------------------------------\n"
-    for_user += "　ユーザ名　　　 : " + data['club_account'] + "\n"
+    for_user += "　ユーザ名　　　 : " + session['club_account'] + "\n"
     for_user += "　-------------------------------------------------------------\n"
-    for_user += "　申請日時　　　 : " + datetime.now(timezone.utc).strftime('%Y/%m/%d %H:%M:%S %z') + "\n"
-    for_user += "　アクセス元　　 : " + gethostname() + "(" + request.remote_addr + ")\n"
+    for_user += "　申請日時　　　 : " + datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S %z') + "\n"
+    for_user += "　アクセス元　　 : " + gethostname() + " (" + request.remote_addr + ")\n"
     for_user += "=================================================================\n"
     for_user += "\n"
     for_user += "これらの申請内容に間違いがなければ、申請日時から <<30分以内>> に\n"
@@ -111,10 +110,10 @@ def mail():
     for_user += "=================================================================\n"
     for_user += "                   Copyright (C) 2016 Kentaro OISHI / K.I.T. SANC\n"
 
-    to_addr = session['isc_account'] + "@mail.kyutech.jp"
-    subject = 'Account Request Validation'
+    to_addr = "lan2014@club.kyutech.ac.jp"
+    subject = "Account Request Validation"
     msg = create_msg(FROM_ADDR, to_addr, ENCODING, subject, for_user)
-    send_msg(FROM_ADDR, to_addr, msg)
+    send_msg(FROM_ADDR, to_addr, msg, SMTP_SRV, 25)
 
     return template('mail')
 
